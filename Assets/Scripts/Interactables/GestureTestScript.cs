a881fcae566914102cdf68a77c2619d470d483b6
xpxchxcx <118047971+xpxchxcx@users.noreply.github.com>
2025-11-05 23:20:12 +0800

    feat: made basic gestures fire text events

---
+++ Assets/Scripts/Interactables/GestureTestScript.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class GestureTestScript : MonoBehaviour
+{
+
+    public void printGestureConsole(string s)
+    {
+        UnityEngine.Debug.Log(s);
+    }
+}
